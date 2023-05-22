0b0930e8a0c544ec185ebf6a25f9cf20276435a3
Martin Gyurkó <nospam@gyurma.de>
2023-05-22 11:23:02 +0200

    last ino to get something going on the arduino dev. ide

--- Firmware/VideoTest/VideoTest.ino
+++ Firmware/VideoTest/VideoTest.ino
@@ -12,79 +12,126 @@ Needed to tie pin 9 to 10 since the different FRQs can only be handled by separa
 RP2040_PWM* PWM_ClkInstance;
 RP2040_PWM* PWM_StInstance;
 
-float frequency2 = 2000; // Um das zu erreichen, braucht Oszillator 125MHz langsam zu sein.
-float frequency = 5000000;
+float STfrequency = 2000; // Um das zu erreichen, braucht Oszillator 125MHz langsam zu sein.
+float Clkfrequency = 5000000;
 
-float dutyCycle2 = 20;
-float dutyCycle = 50;
+float STDutyCycle = 10;
+float ClkDutyCycle = 50;
 
 #define pinToUse      25 
 #define ClkPin        10
 #define StPin         8
+#define debugPin      19
 
+#define ADCIn1        5
+#define ADCIn2        4
+#define ADCIn3        3
+#define ADCIn4        2
+
+
+
+int ADCSum;
+int ClkCounter = 0;
+boolean STStatus;
+boolean STStatusOld = false;
+boolean ClkStatus;
+boolean ClkStatusOld = false;
+String ADCResults = {"Testpattern. This should be overwritten after first capture."};
 
 void setup()
 {
 
-  PWM_StInstance = new RP2040_PWM(StPin, frequency2, dutyCycle2);
-  PWM_ClkInstance = new RP2040_PWM(ClkPin, frequency, dutyCycle);
+  pinMode(ADCIn1,INPUT);
+  pinMode(ADCIn2,INPUT);
+  pinMode(ADCIn3,INPUT);
+  pinMode(ADCIn4,INPUT);
+  pinMode(pinToUse,OUTPUT);
+  pinMode(debugPin, OUTPUT);
+  
+
+
+}
+
+void loop()
+{
+
+  ClkStatus = digitalRead(ClkPin);
+  STStatus = digitalRead(StPin);
+//  digitalWrite(pinToUse,STStatus);
+//  digitalWrite(debugPin,ClkStatus);
+  if ((STStatus == false ) && (STStatusOld == true )){
+  digitalWrite(pinToUse,HIGH);}
+  else
+  digitalWrite(pinToUse,LOW);
+
+/*    Serial.print(STStatus);
+    Serial.print(STStatusOld);
+    Serial.print(ClkStatus);
+    Serial.print(ClkStatusOld);
+    Serial.println(ClkCounter);
+
+ //   Serial.println(ADCResults);
+    ClkCounter = 0;
+  }
+*/  
+  if ((ClkStatusOld == false) && (ClkStatus == true)){
+  digitalWrite(debugPin,HIGH);}
+  else
+  digitalWrite(debugPin,LOW);
+/*
+
+    if ((ClkCounter > 50)&&(ClkCounter < 2547)){
+      ADCSum =  digitalRead(ADCIn1)+digitalRead(ADCIn2)+digitalRead(ADCIn3)+digitalRead(ADCIn4)+0x30;
+      ADCResults[ClkCounter-50] = ADCSum;
+    }
+    
+    ClkCounter++;
+    if (ClkCounter >= 2546)
+     Serial.print(ClkCounter);
+  }
+*/
+  ClkStatusOld = ClkStatus;
+  STStatusOld = STStatus;
+}
+
+
+
+void setup1()
+{
+  PWM_StInstance = new RP2040_PWM(StPin, STfrequency, STDutyCycle);
+  PWM_ClkInstance = new RP2040_PWM(ClkPin, Clkfrequency, ClkDutyCycle);
 
   PWM_StInstance->setPWM();
   PWM_ClkInstance->setPWM();
   // initialize serial:
   Serial.begin(115200);
-  while (Serial.available()) 
-    Serial.read(); //just to flush first the buffer
-}
-
-void loop()
-{
-  // print the string when a newline arrives:
-  while (Serial.available()) {
-
-    frequency = Serial.parseFloat();
-    dutyCycle = Serial.parseFloat();
-    frequency2 = Serial.parseFloat();
-    dutyCycle2 = Serial.parseFloat();
-  
-    Serial.print(frequency);
-    Serial.print(' ');
-    Serial.print(dutyCycle);
-    Serial.print(' ');
-    Serial.print(frequency2);
-    Serial.print(' ');
-    Serial.println(dutyCycle2);
-    
-    PWM_ClkInstance->setPWM(ClkPin, frequency, dutyCycle);
-    PWM_StInstance->setPWM(StPin, frequency2, dutyCycle2);
-    
-    while(Serial.available()) {
-      Serial.read();
-      };
-
-
-    
-   } 
-
-}
-
-/*
-
-void setup1()
-{
-   //assigns pin 25 (built in LED), with frequency of 20 KHz and a duty cycle of 0%
-  PWM_Instance = new RP2040_PWM(pinToUse, 200000, 40);
-  PWM_Instance->setPWM();
-  
 }
 
 void loop1()
 {
   
-  PWM_Instance->setPWM(pinToUse, 200000, 10);
-  delay(1000);
-  PWM_Instance->setPWM(pinToUse, 200000, 90);
-  delay(1000);
+  // print the string when a newline arrives:
+  while (Serial.available()) {
+
+    Clkfrequency = Serial.parseFloat();
+    ClkDutyCycle = Serial.parseFloat();
+    STfrequency = Serial.parseFloat();
+    STDutyCycle = Serial.parseFloat();
   
+    Serial.print(Clkfrequency);
+    Serial.print(' ');
+    Serial.print(ClkDutyCycle);
+    Serial.print(' ');
+    Serial.print(STfrequency);
+    Serial.print(' ');
+    Serial.println(STDutyCycle);
+    
+    PWM_ClkInstance->setPWM(ClkPin, Clkfrequency, ClkDutyCycle);
+    PWM_StInstance->setPWM(StPin, STfrequency, STDutyCycle);
+    
+    while(Serial.available()) {
+      Serial.read();
+      };
+   } 
+
 }
-*/
