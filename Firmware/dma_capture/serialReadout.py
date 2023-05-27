f06fb9bf49066e966262b14899e73f2013f481cd
Martin Gyurko <m.gyurko@mm1.com>
2023-05-27 09:45:50 +0200

    nice visualisation

--- Firmware/dma_capture/serialReadout.py
+++ Firmware/dma_capture/serialReadout.py
@@ -1,10 +1,12 @@
 import serial
 import time
 from matplotlib import pyplot as plt
+import numpy as np
 
 s = serial.Serial('COM6')  #adjust port to your needs
 plt.ion()
 message = "2700\n"
+first = True
 while(True) :
     s.write(message.encode())
 
@@ -28,16 +30,37 @@ while(True) :
  
     t= range(len(res))
     a= list(res)
-
+    t = t[174:-53]
+    a= a[174:-53]
+    if first :
+        aold = a
+        amin = a
+        c = a
+        first = False
   #  print(a)
     plt.cla()
-    plt.plot(t[11:-10],a[11:-10],visible=True)
+    plt.plot(t,a)
     xmin,xmax,ymin,ymax = plt.axis()
-    plt.show()
-    plt.pause(0.1)
     print(len(a) )
     print(xmin,xmax,ymin,ymax)
-
+    # print(a[174:-53])
+    # b = np.convolve(a,aold)
+    # plt.plot(b/100000)
     # time.sleep(0.5)
+    
+    for i in range(len(a)):
+        if (a[i] < amin[i]) :
+            amin[i] = a[i]   
+    plt.plot(t, amin)
+    print(amin[0:20])
+
+    for j in range(len(a)):
+        c[j] = a[j] - amin[j]   
+    print( c[0:20])
+
+    # plt.plot(t, c)
+    aold = a
+    plt.show()
+    plt.pause(1)
 
 s.close()
