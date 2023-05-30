2e2742c8aa67f282002e5d0fd2885ea4b1d1cffa
Martin Gyurko <m.gyurko@mm1.com>
2023-05-31 00:04:19 +0200

    Erste Python Positionsergebnisse

--- Firmware/dma_capture/code/serialReadout.py
+++ Firmware/dma_capture/code/serialReadout.py
@@ -4,9 +4,15 @@ from matplotlib import pyplot as plt
 import numpy as np
 from calibrationfile import amin, amax
 
-aold = []
+calibrating = False
 
+position = 0
+
+aold = []
+k=0.995
 c = []
+cavg = []
+cold = []
 counter = 0
 s = serial.Serial('COM6')  #adjust port to your needs
 plt.ion()
@@ -29,7 +35,7 @@ while(True) :
 
     res = s.readline()
 
-    print(res.decode() )
+    # print(res.decode() )
 
     res = s.readline()
  
@@ -38,19 +44,21 @@ while(True) :
     t = t[180:-53]
     a= a[180:-53]
     if first :
-        amax = a.copy()
+        # amax = a.copy()
         aold = a.copy()
-        amin = a.copy()
+        # amin = a.copy()
         c = a.copy()
+        cold = a.copy()
+        cavg = a.copy()
         first = False
         print(first)
 
     # print(a)
-    plt.cla()
-    plt.plot(t,a)
-    xmin,xmax,ymin,ymax = plt.axis()
-    print(len(a) )
-    print(xmin,xmax,ymin,ymax)
+    # plt.cla()
+    # plt.plot(t,a)
+    # xmin,xmax,ymin,ymax = plt.axis()
+    # print(len(a) )
+    # print(xmin,xmax,ymin,ymax)
     # print(a[174:-53])
     # b = np.convolve(a,aold)
     # plt.plot(b/100000)
@@ -58,43 +66,55 @@ while(True) :
 
     # print(a[0:40])
     # print(amin[0:40])
-    
-    for i in range(len(a)):
-        if (a[i] < amin[i]) :
-            amin[i] = a[i]
-        if (a[i] > amax[i]) :
-            amax[i] = a[i]
-    plt.plot(t, amin)
-    plt.plot(t, amax)
+    if (calibrating) :
+        for i in range(len(a)):
+            if (a[i] < amin[i]) :
+                amin[i] = a[i]
+            if (a[i] > amax[i]) :
+                amax[i] = a[i]
+    # plt.plot(t, amin)
+    # plt.plot(t, amax)
 
     # print(amin[0:40])
 
+
     for j in range(len(a)):
+        cold[j] = c[j]
         c[j] = (a[j] - amin[j])
+        # cavg[j] = k*cavg[j]+(1-k)*c[j]
         #print(c[j], a[j],amin[j], end=" : ")   
     # print( c[0:40])
     # print(amin[0:40])
 
-    plt.plot(t, c)
+    # plt.plot(t, c)
+    # plt.plot(t, cold)
+    # plt.plot(t, cavg)
 
     # f = np.fft.fft(c)
     # print(np.abs(f)[1:20])
     
     # plt.plot(t, np.abs(f))
     # plt.plot(t, g)
+    d = np.correlate(c,cold,mode="full")
+    
+    # plt.plot(d/5000)
+    
+    
 
+    delta = (int) (np.argmax(d)-len(d)/2)
+    position = position + delta
+    print("%d %8d %8d"%(len(d), delta, position), end="\r")
 
-    aold = a
-    plt.show()
-    plt.pause(0.01)
-
-    if (counter > 600):
-      break
-    else:
-        counter = counter +1
+    # aold = a
+    # plt.show()
+    # plt.pause(0.01)
+    # if (counter > 600):
+    #   break
+    # else:
+    #     counter = counter +1
 
 s.close()
 
-print("amin = ", amin)
-print("amax = ", amax)
+# print("amin = ", amin)
+# print("amax = ", amax)
 
